0a41e6f54aeb7690fdaf762e1c08ed031f8d5138
JuliaKh <myrashkam8@mail.ru>
2018-11-13 23:42:49 +0200

    update

---
+++ 3.Weather Forecast/insert.js
@@ -0,0 +1,51 @@
+class getElement {
+  constructor(selector) {
+    this.elem = document.querySelector(selector);
+  }
+  inner(value) {
+	return this.elem.innerHTML = value;
+  }
+};
+
+
+function showWeather(weatherMinsk) {          
+  var tempMinsk = getCelsius(weatherMinsk.list[0].main.temp);
+  var iconMinsk = weatherMinsk.list[0].weather[0].icon;
+  var minTempMinsk = getCelsius(weatherMinsk.list[0].main.temp_min);
+  var maxTempMinsk = getCelsius(weatherMinsk.list[0].main.temp_max);
+  var temperature = tempMinsk + `<span>&deg;C</span>`;
+  var icon = "<img src = https://openweathermap.org/img/w/" + iconMinsk + ".png>";
+  var clauds = weatherMinsk.list[0].weather[0].description;
+  var humidity = weatherMinsk.list[0].main.humidity + " %";
+  var wind = weatherMinsk.list[0].wind.speed + " m/s"; 
+  var minTemp = "Min t " + minTempMinsk + " &deg;";
+  var maxTemp = "Max t " + maxTempMinsk + " &deg;"; 
+
+  new getElement('.tempMinsk').inner(temperature);
+  new getElement('.iconMinsk').inner(icon);
+  new getElement('.claudsMinsk').inner(clauds);
+  new getElement('.humidityMinsk').inner(humidity);
+  new getElement('.windMinsk').inner(wind);          
+  new getElement('.temp_minMinsk').inner(minTemp);
+  new getElement('.temp_maxMinsk').inner(maxTemp);
+}  
+
+
+function getAllDayForecast(data) {
+  var hourInterval = 24 / 3;
+  var result = '';
+
+  for (var i = 0; i < hourInterval; i++) {
+      var time = (data.list[i].dt_txt).slice(11,16);
+      var icon = data.list[i].weather[0].icon;
+      var temp = getCelsius(data.list[i].main.temp);
+      
+      result += `
+	     <div class="block">
+	        <div class="time_out">${time}</div>
+	        <img src = https://openweathermap.org/img/w/${icon}.png>
+	        <div>${temp}<span>&deg;C</span></div>
+	     </div>`;
+  }
+  new getElement('.all_dayForecast').inner(result);     
+} 
--- 4.Family Tree/index.js
+++ 4.Family Tree/index.js
@@ -3,99 +3,107 @@ import {data} from './data.js';
 var out = document.querySelector('.wrapper');
 var newData = data;
 
-//check if we have saved data
+
 if (localStorage.getItem('tree') != undefined) {
-    newData = JSON.parse(localStorage.getItem('tree'));
-  } 
+   newData = JSON.parse(localStorage.getItem('tree'));
+} 
+
 
 showTree(newData);
   
 
 function showTree(data) {
-    var result = createUl(createTree(data));
-    out.appendChild(result);
+  var result = createUl(createTree(data));
+  out.appendChild(result);
 }
 
+
 function createTree(data) {
-    for (var key in data){
-        if (key === 'name') {
-            var li = createLi(data.name);       
-        } else if (key === 'children') {
-            var childArr = data[key];
-            var ul = document.createElement('ul'); 
-            childArr.reduce((prev, curr) => {                    
-                var  children = createTree(curr);                      
-                prev.appendChild(children);                                             
-                return ul;                        
-            },ul);             
-            if(ul.innerHTML !== '') li.appendChild(ul);                                 
-        }
+    for (var key in data) {
+      if (key === 'name') {
+         var li = createLi(data.name);       
+      } else if (key === 'children') {
+         var childArr = data[key];
+         var ul = document.createElement('ul');
+
+         childArr.reduce((prev, curr) => {                    
+            var  children = createTree(curr);                      
+            prev.appendChild(children);                                             
+            return ul;                        
+         }, ul); 
+
+        if (ul.innerHTML !== '') {
+           li.appendChild(ul);
+        }                                 
+      }
     }
     return li; 
 }
 
 
- function createLi(name) { 
-    var li = document.createElement('li'); 
-    var span = document.createElement('span'); 
-    var button = document.createElement('button');
-    var input = document.createElement('input');
+function createLi(name) { 
+  var li = document.createElement('li'); 
+  var span = document.createElement('span'); 
+  var button = document.createElement('button');
+  var input = document.createElement('input');
 	
-    button.innerHTML = 'add' ;    
-    span.innerHTML = name;
-    li.appendChild(span);
-    li.appendChild(input);
-    li.appendChild( button);
+  button.innerHTML = 'add';    
+  span.innerHTML = name;
+  li.appendChild(span);
+  li.appendChild(input);
+  li.appendChild(button);
 	
-    return li;      
+  return li;      
 }
 
 function createUl(li) { 
-    var ul = document.createElement('ul');
-	
-    ul.appendChild(li);
-    return ul;      
+  var ul = document.createElement('ul');
+
+  ul.appendChild(li);
+  return ul;      
 }
 
 //add Values
 out.onclick = function addValue(event) {
-    var target = event.target;
+  var target = event.target;
 
-    if (target.tagName != 'BUTTON') return;
+  if (target.tagName != 'BUTTON') return;
     
-    var input = target.previousSibling;
-    var inputValue = input.value;
-    var span = input.previousSibling;
-    var spanValue = span.innerHTML;
+  var input = target.previousSibling;
+  var inputValue = input.value;
+  var span = input.previousSibling;
+  var spanValue = span.innerHTML;
 
-    pushValues(newData, spanValue, new Object(inputValue));
-    localStorage.setItem('tree', JSON.stringify(newData));
-    window.location.reload();
+  pushValues(newData, spanValue, new Object(inputValue));
+  localStorage.setItem('tree', JSON.stringify(newData));
+  window.location.reload();
 }
 
 
 class Object {
-   constructor(name) {
-       this.name = name,
-       this.children = []
-   }
+  constructor(name) {
+    this.name = name,
+    this.children = []
+  }
 }
+
 
 function pushValues(obj, match, newVal) {
-    for (var key in obj) {
-        if (key === 'name'){
-            if (obj[key] === match) {
-                var outPut = obj.children;
-                outPut.push(newVal);
-                return;
-            }       
-         } else if (key === 'children') {
-            obj[key].forEach( x =>  pushValues(x, match, newVal));
-        }
+  for (var key in obj) {
+    if (key === 'name') {
+      if (obj[key] === match) {
+         var outPut = obj.children;
+         outPut.push(newVal);
+         return;
+      }       
+    } else if (key === 'children') {
+         obj[key].forEach( x =>  pushValues(x, match, newVal));
     }
+  }
 }
 
+
 document.querySelector('.skip').onclick = function() {
-    localStorage.clear();
-    window.location.reload();
+  localStorage.clear();
+  window.location.reload();
 }
