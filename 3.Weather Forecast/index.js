50c06c8121060665e30612b6a11656bd01d350eb
JuliaKh <myrashkam8@mail.ru>
2018-11-16 22:21:11 +0200

    add module

--- 3.Weather Forecast/index.js
+++ 3.Weather Forecast/index.js
@@ -1,24 +1,25 @@
-var endpointMinsk ='https://api.openweathermap.org/data/2.5/forecast?id=625144&APPID=9117bf1c0fa1bf432322023751327cc8';
+var f = (function() {
+  var endpointMinsk = 'https://api.openweathermap.org/data/2.5/forecast?id=625144&APPID=9117bf1c0fa1bf432322023751327cc8';
 
-fetch(endpointMinsk)
-  .then(blob => blob.json())
-  .then(data => { 
-      showWeather(data);
-      getAllDayForecast(data);
-  });
-
-
-function getCelsius(val) {
-  var result = Math.floor(val - 273);
-  return result;
-}
-
-
-class getElement {
-  constructor(selector) {
-    this.elem = document.querySelector(selector);
+  function getData() {
+      fetch(endpointMinsk)
+      .then(blob => blob.json())
+      .then(data => { 
+          showWeather(data);
+          getAllDayForecast(data);
+      })
   }
-  inner(value) {
-	return this.elem.innerHTML = value;
+
+
+  function getCelsius(val) {
+    var result = Math.floor(val - 273);
+    return result;
   }
-};
+
+  return {
+    getData: getData,
+    getCelsius: getCelsius
+  }
+})();
+
+f.getData();
